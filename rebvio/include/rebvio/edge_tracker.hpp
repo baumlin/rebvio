00a295396efc89f0c36b0d30111541b7dec554e9
baumlin <nathanbaumli@gmail.com>
2018-11-06 09:01:12 +0100

    (rebvio): remove kagmebias class from edge tracker and replace with sab estimator

--- rebvio/include/rebvio/edge_tracker.hpp
+++ rebvio/include/rebvio/edge_tracker.hpp
@@ -78,39 +78,6 @@ private:
 	rebvio::EdgeMap::SharedPtr map_;
 };
 
-class KaGMEKBias {
-public:
-	struct Config {
-		rebvio::types::Vector3f a_v;    //!< Visual acceleration
-		rebvio::types::Vector3f a_s;    //!< Gravity-corrected acceleration
-		types::Float G;                 //!< Standard gravity (norm of gravitational acceleration)
-		rebvio::types::Vector7f x_p;    //!< Prior filter state
-		rebvio::types::Matrix7f Pp;     //!< Prior filter state covariance
-		rebvio::types::Matrix3f Rv;     //!< Observation noise of visual acceleration
-		rebvio::types::Matrix3f Rs;     //!< Observation noise of gravity-corrected acceleration
-		types::Float Rg;                //!< Observation noise of standard gravity
-
-		Config(const rebvio::types::Vector3f& _a_v, const rebvio::types::Vector3f& _a_s, types::Float _G, const rebvio::types::Vector7f& _x_p,
-					 const rebvio::types::Matrix3f& _Rv, const rebvio::types::Matrix3f& _Rs, types::Float _Rg, const rebvio::types::Matrix7f& _Pp) :
-						 a_v(_a_v), a_s(_a_s), G(_G), x_p(_x_p), Rv(_Rv), Rs(_Rs), Rg(_Rg), Pp(_Pp) {}
-		Config() = delete;
-	};
-
-public:
-	KaGMEKBias(KaGMEKBias::Config& _config);
-	KaGMEKBias() = delete;
-	~KaGMEKBias();
-	bool problem(rebvio::types::Matrix7f& _JtJ, rebvio::types::Vector7f& _JtF, const rebvio::types::Vector7f& _X);
-	int gaussNewton(rebvio::types::Vector7f& _X, int _iter_max, types::Float _a_tol = 0.0, types::Float _r_tol = 0.0);
-
-private:
-	inline types::Float saturate(types::Float _t, types::Float _limit);
-
-private:
-	KaGMEKBias::Config config_;
-
-};
-
 
 struct EdgeTrackerConfig {
 	types::Float search_range{40.0};                 //!< Pixel search range for tracking and mapping
