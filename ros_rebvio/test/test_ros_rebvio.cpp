fa6826ab772e67653ff6bab09280e9d94640eae0
baumlin <nathanbaumli@gmail.com>
2018-11-19 16:33:20 +0100

    (ros_rebvio): add missing test source file

---
+++ ros_rebvio/test/test_ros_rebvio.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "rebvio/rebvio.hpp"
+
+
+
+TEST(RegressionTest, odometry) {
+
+}
+
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc,argv);
+	return RUN_ALL_TESTS();
+}
